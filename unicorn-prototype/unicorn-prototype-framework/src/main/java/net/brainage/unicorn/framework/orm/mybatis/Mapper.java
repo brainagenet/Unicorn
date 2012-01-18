9f6ad15ddeae6fbe01f92a8cb17d75cadd14e8e3
exia-x420 <ms29.seo@gmail.com>
2012-01-19 00:09:15 +0900

    Initial Import

---
+++ unicorn-prototype/unicorn-prototype-framework/src/main/java/net/brainage/unicorn/framework/orm/mybatis/Mapper.java
@@ -0,0 +1,16 @@
+/*
+ * (#) net.brainage.unicorn.framework.orm.mybatis.Mapper.java
+ * Created on 2012. 1. 19.
+ */
+package net.brainage.unicorn.framework.orm.mybatis;
+
+/**
+ * MyBatis Mapper Marker Interface
+ *
+ * @author ms29.seo@gmail.com
+ * @version 1.0
+ */
+public interface Mapper
+{
+
+}
---
+++ unicorn-prototype/unicorn-prototype-framework/src/main/java/net/brainage/unicorn/framework/orm/mybatis/annotation/Mapper.java
@@ -0,0 +1,19 @@
+/*
+ * (#) net.brainage.unicorn.framework.orm.mybatis.annotation.Mapper.java
+ * Created on 2012. 1. 19.
+ */
+package net.brainage.unicorn.framework.orm.mybatis.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * 
+ *
+ * @author ms29.seo@gmail.com
+ * @version 1.0
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Mapper
+{
+}
