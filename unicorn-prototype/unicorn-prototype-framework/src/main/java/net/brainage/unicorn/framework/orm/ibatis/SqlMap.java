40185960bee65f604f4576e1c16378e3043445f3
exia-x420 <ms29.seo@gmail.com>
2012-01-20 00:44:40 +0900

    Initial Import - iBatis(MyBatis 2.3.5) Marker Interface and Marker Annotation

---
+++ unicorn-prototype/unicorn-prototype-framework/src/main/java/net/brainage/unicorn/framework/orm/ibatis/SqlMap.java
@@ -0,0 +1,16 @@
+/*
+ * (#) net.brainage.unicorn.framework.orm.ibatis.SqlMap.java
+ * Created on 2012. 1. 20.
+ */
+package net.brainage.unicorn.framework.orm.ibatis;
+
+/**
+ * iBatis(MyBatis 2.3.5) SqlMap Marker Interface
+ *
+ * @author ms29.seo@gmail.com
+ * @version 1.0
+ */
+public interface SqlMap
+{
+
+}
---
+++ unicorn-prototype/unicorn-prototype-framework/src/main/java/net/brainage/unicorn/framework/orm/ibatis/annotation/SqlMap.java
@@ -0,0 +1,20 @@
+/*
+ * (#) net.brainage.unicorn.framework.orm.ibatis.annotation.SqlMap.java
+ * Created on 2012. 1. 20.
+ */
+package net.brainage.unicorn.framework.orm.ibatis.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * 
+ *
+ * @author ms29.seo@gmail.com
+ * @version 1.0
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SqlMap
+{
+
+}
